22b3817a11bb4a8dbe30b10f44a9b52704d6b2eb
Your Name <you@example.com>
2016-11-08 22:49:32 +0800

    perf indicator test

--- app/src/androidTest/java/com/example/dbman/ui/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/dbman/ui/ExampleInstrumentedTest.java
@@ -4,17 +4,24 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.apkfuns.logutils.LogUtils;
 import com.example.dbman.core.BaseApplication;
 import com.example.dbman.core.BaseDatabase;
 import com.example.dbman.db.core.Database;
+import com.example.dbman.db.gen.Order;
 import com.example.dbman.db.genupdate.daoimpl.EquipTypeDaoImpl;
 import com.example.dbman.db.genupdate.schema.EquipType;
+import com.example.dbman.db.genupdate.schema.SysParameter;
 import com.example.dbman.ui.PowerIndicator.EquipHirarchyModel;
+import com.j256.ormlite.dao.CloseableIterator;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.List;
+import java.util.UUID;
 
 import dalvik.annotation.TestTargetClass;
 
@@ -43,9 +50,25 @@ public class ExampleInstrumentedTest {
             EquipHirarchyModel.getInstance();
             assertTrue(true);
             EquipTypeDaoImpl impl =(EquipTypeDaoImpl)BaseDatabase.getInstance().getDaoImpl("EquipType");
+            //
             List<EquipType> lsts = impl.findBySimilarTypeName("车");
             assertTrue(lsts.size() == 311);
+            //综合测试1
+            {
+                CloseableIterator<EquipType> iter1 = impl.lookupBriefEquipTypeInfo(impl.getLeafEquipByParentUUIDQuery(UUID.fromString("0FC40DF9-6534-44DA-9018-00EF24CA5E3F")));
+                while(iter1.hasNext()){
+                    EquipType equipType = iter1.nextThrow();
+                    LogUtils.e(equipType);
+                }
+                lsts = impl.findBySimilarTypeName("越野车");
+                iter1 = impl.lookupBriefEquipTypeInfo(impl.getLeafEquipByParentObjectsQuery(lsts.iterator()));
+                while(iter1.hasNext()){
+                    EquipType equipType = iter1.nextThrow();
+                    LogUtils.e(equipType);
+                }
+            }
         }catch (Exception e){
+            e.printStackTrace();
             assertFalse(true);
         }
     }
--- app/src/main/java/com/example/dbman/db/genupdate/daoimpl/EquipTypeDaoImpl.java
+++ app/src/main/java/com/example/dbman/db/genupdate/daoimpl/EquipTypeDaoImpl.java
@@ -1,7 +1,16 @@
 package com.example.dbman.db.genupdate.daoimpl;
+import com.example.dbman.core.BaseDatabase;
+import com.example.dbman.core.Constants;
+import com.example.dbman.db.genupdate.schema.SysParameter;
+import com.j256.ormlite.dao.CloseableIterable;
+import com.j256.ormlite.dao.CloseableIterator;
+import com.j256.ormlite.dao.CloseableWrappedIterable;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.BaseDaoImpl;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,12 +36,61 @@ public class EquipTypeDaoImpl extends BaseDaoImpl<EquipType,java.util.UUID> impl
 
 	@Override
 	public List<EquipType> findBySimilarTypeName(String typeName) throws SQLException{
+		return findBySimilarTypeNameQuery(typeName).query();
+	}
+
+	@Override
+	public Where<EquipType,UUID> getLeafEquipByParentUUIDQuery(UUID ... parentUUIDs) throws SQLException {
+		final Where<EquipType,UUID> nullWhere = queryBuilder.where().in("PkTypeID", Constants.NULL_UUID);
+		HashMap<UUID,UUID> uuids = new HashMap<UUID,UUID>();
+		for (UUID parentUUID:parentUUIDs) {
+			tryAdd(uuids, parentUUID);
+		}
+		return (uuids.size()>0)?queryBuilder.where().in("PkTypeID", uuids):nullWhere;
+	}
+	@Override
+	public Where<EquipType,UUID> getLeafEquipByParentObjectsQuery(Iterator<EquipType> iterator) throws SQLException{
+		final Where<EquipType,UUID> nullWhere = queryBuilder.where().in("PkTypeID", Constants.NULL_UUID);
+		HashMap<UUID,UUID> uuids = new HashMap<UUID,UUID>();
+		while (iterator.hasNext()){
+			tryAdd(uuids, iterator.next().getPkTypeID());
+		}
+		return (uuids.size()>0)?queryBuilder.where().in("PkTypeID", uuids):nullWhere;
+	}
+
+	private  void tryAdd(HashMap<UUID,UUID>uuids, final UUID pId) throws SQLException{
+		List<EquipType> subs = findByParentUUID(pId);
+		if ( (subs == null) || (subs.size() <= 0) ){
+			uuids.put(pId,pId);
+		}else{
+			for (EquipType equipType:subs){
+				tryAdd(uuids, equipType.getPkTypeID());
+			}
+		}
+	}
+
+	@Override
+	public Where<EquipType,UUID> findBySimilarTypeNameQuery(String typeName) throws SQLException {
 		String arg;
 		if (typeName.length()> 0){
-		 	arg = "%"+typeName+"%";
+			arg = "%"+typeName+"%";
 		}else{
 			arg = "%";
 		}
-		return queryBuilder.where().like("TypeName", arg).query();
+		return  queryBuilder.where().like("TypeName", arg);
+	}
+
+	@Override
+	public CloseableIterator<EquipType> lookupBriefEquipTypeInfo(Where<EquipType,UUID> query)  throws SQLException {
+		SysParameterDaoImpl sysParameterDao = (SysParameterDaoImpl)BaseDatabase.getInstance().getDaoImpl("SysParameter");
+		QueryBuilder<SysParameter, UUID> sysParameterUUIDQueryBuilder = sysParameterDao.queryBuilder();
+		QueryBuilder<EquipType,UUID> joinedQB = queryBuilder.join(sysParameterUUIDQueryBuilder);
+		joinedQB.setWhere(query);
+		return joinedQB.iterator();
+	}
+
+	@Override
+	public CloseableIterator<EquipType> lookupDetailedEquipTypeInfo(Where<EquipType,UUID> query) throws SQLException{
+		return null;
 	}
 }
